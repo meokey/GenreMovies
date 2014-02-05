6f0db84c778fc845bee7fa34bd53c6b183ce3c3e
meokey <meokey2000@gmail.com>
2014-02-05 15:32:43 -0500

    Essential function works; Ignore list

--- GM.py
+++ GM.py
@@ -1,6 +1,7 @@
-from os import listdir, symlink
-from os.path import isfile, isdir, islink, join, split
-import errno, sys
+from os import listdir, symlink, link
+from os.path import isfile, isdir, islink, join, split, getsize
+from glob import glob
+import errno, sys, getpass, codecs
 import re, pprint, json, ast
 
 from xml.dom.minidom import parseString, Document
@@ -16,6 +17,12 @@ class ppprint(pprint.PrettyPrinter):
             return (object.encode('utf8'), True, False)
         return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
 
+def unic(s):
+	if type(s) == str:
+		v = unicode(s, "utf-8", errors="ignore")
+	else:
+		v = unicode(s)
+	return v
 
 def querymovie(*args, **kwargs):
 	title = kwargs.get('title', '')
@@ -32,13 +39,16 @@ def querymovie(*args, **kwargs):
 		res = omdb.get(imdbid=imdbid)
 	else:
 		res = omdb.get(title=title,year=year)
-	
+	#print res	# comment
 	if len(res)>0:
 		return res
-	else:
+	elif title:
 		return searchmovie(title, year)
+	else:
+		return ''
 
 def searchmovie(title, year):
+
 	aa = omdb.Client()
         movies = parseString(aa.request(s=title+'*', r='XML').content).getElementsByTagName('Movie')
         if movies.length>0:
@@ -74,7 +84,7 @@ def moviename(d):
 	title = d
 	year = ''
 	
-	aaa = '720p|1080p|dvdrip|dbd-rip|bdrip|brrip|bluray|h264|x264|xvid|ntsc|pal|hq|dts|hdtv|aac'
+	aaa = '720p|1080p|dvdrip|dbd-rip|bdrip|brrip|bluray|blu-ray|h264|x264|xvid|ntsc|pal|hq|dts|hdtv|aac|duology|trilogy|quadrilogy|pentalogy|hexology|heptalogy|octology|anthology|extended|pdvd'
 	
 	#print '###moviename###	'+d		#comment	
 	tt = re.search(r'(tt\d{7})',d)
@@ -82,7 +92,7 @@ def moviename(d):
                 imdbid = tt.group(1)
         else:
                 if str.isalnum(d[0]):
-			my = re.search(r'^(.*?)\.\((19|20\d{2})[0-9,-]*\).*',d)
+			my = re.search(r'^(.*?)\.\(((19|20)\d\d)[0-9,-]*\).*',d)
                 	if my:
                         	title = my.group(1)
 				year = my.group(2)
@@ -91,14 +101,14 @@ def moviename(d):
                         	if m:
                                 	title = m.group(1)
                         	else:
-                                	m = re.search(r'^(.*?)\.'+aaa,d,re.I)
+                                	m = re.search(r'^(.*?)\.?('+aaa+')',d,re.I)
                                 	if m:
                                         	title = m.group(1)
 		else:
-			my = re.search(ur'^.*\.\(((19|20)\d\d)[0-9,-]*\)\.(.*?)'+aaa,d,re.I)
+			my = re.search(ur'^.*\.\(((19|20)\d\d)[0-9,-]*\)\.(.*?)\.?('+aaa+')',d,re.I)
 			if my:
-				title = my.group(1)
-				year = my.group(3)
+				title = my.group(3)
+				year = my.group(1)
 			else:
 				my = re.search(ur'^.*\.\(((19|20)\d\d)[0-9,-]*\)\.(.*)$',d,re.I)
 				if my:
@@ -107,36 +117,58 @@ def moviename(d):
 
 	return (title, year, imdbid)
 
-def searchdir(movies_repo,opt_display):
+def searchdir(movies_repo,opt_display,ignore):
 	y = {}
 	n = {}
 
 	for d in listdir(movies_repo):
+		if d in ignore:
+			print'\tIgnoring movie ['+d+']...'
+			continue
+
 		md = join(movies_repo,d)
 		if isdir(md):
-			#print d	# comment
-	
-			if isfile(md+'/'+d+'.imdb.xml'):	# existance of imdb.xml indicates that links have been created, skip
+			#print '\t####searchdir: '+md	#comment
+			if isfile(md+'/imdb.xml'):	# existance of imdb.xml indicates that links have been created, skip
+				print '\tSkipping movie ['+d+']...'
 				continue
+			#print "\tI'm processing movie ["+d+'].'  # comment
 
 			# check omdbapi.com to gather imdb info for the movie
 			title, year, imdbid = moviename(d)					
-			res = querymovie(title=title, year=year,imdbid=imdbid)
+			res = querymovie(title=title, year=year, imdbid=imdbid)
 
-			if len(res)>0:
-				#print u'Movie:\t'+res['title'].decode("utf-8")+u', '+res['year'].decode("utf-8")+u', '+res['imdb_id'].decode("utf-8")+u', '+res['genre'].decode("utf-8")	# comment
+			if len(res)>2:
+				#print u'\tMovie:\t'+res['title'].decode("utf-8")+u', '+res['year'].decode("utf-8")+u', '+res['imdb_id'].decode("utf-8")+u', '+res['genre'].decode("utf-8")	# comment
 				
-				#xmlfile = movies_repo+d+'/'+res['title']+'.('+res['year']+')'+'.'+res['imdb_id']+'.imdb.xml'
-				xmlfile = md+'/'+d+'.imdb.xml'
-				if (not isfile(xmlfile)) and (not opt_display):
-					f = open(xmlfile,'w')
-					f.write(parseString(re.sub(u'root',u'movie',dicttoxml.dicttoxml(res).encode('utf-8'))).toprettyxml())
-					f.close()
-				genres = res['genre']
-				genres = genres.replace(" ","").split(",")
+				xmlfile = md+'/'+res['title'].replace('/',' ').replace('*',' ').replace('?',' ').encode("utf-8")+'.('+res['year'].encode("utf-8")+').'+res['imdb_id'].encode("utf-8")+'.imdb.xml'
+				#print 'xmlfile = ['+xmlfile+']'		# comment
+				if not opt_display:		# (not isfile(xmlfile)) and
+					w = True	#False
+					try:
+						s = getsize(xmlfile)
+						if s==0:
+							w = True
+					except (OSError, IOError):
+						w = True
+					if w==True:
+						f = codecs.open(xmlfile,'w', encoding="utf-8")
+						f.write(parseString(re.sub(ur'root',ur'movie',dicttoxml.dicttoxml(unic(res)))).toprettyxml().encode("utf-8"))
+						f.close()
+						link(xmlfile,md+'/imdb.xml')
+				if isfile(md+'/.genres.dat'):	# pre-defined genres
+                                        with open(md+'/.genres.dat','r') as f:
+                                                genres = ast.literal_eval(f.read())
+                                        f.close()
+                                        y[d] = genres
+                                else:				# read genres from res
+					genres = res['genre']
+					if not genres:
+						genres = res['Genre']
+					genres = genres.replace(" ","").split(",")
+					if not opt_display:
+                    	                    json.dump(genres,open(md+'/.genres.dat','w'))
 				y[d] = genres
-				if not opt_display:
-					json.dump(genres,open(md+'/.genres.dat','w'))
 	
 				#xml = re.sub('root','movie',dicttoxml.dicttoxml(res).encode('utf-8'))
 				#print parseString(xml).toprettyxml()
@@ -144,9 +176,10 @@ def searchdir(movies_repo,opt_display):
 				# check .genres.dat file for pre-defined genres to the movie
 	                        if isfile(md+'/.genres.dat'):
         	                        with open(md+'/.genres.dat','r') as f:
-                	                        gerens = ast.literal_eval(f.read())
+                	                        genres = ast.literal_eval(f.read())
 					f.close()
-					y[d] = gerens
+					y[d] = genres
+					n[d.decode('utf-8')] = ''
 				else:
 					#print 'I can not find the movie ['+d+'], or something likewise.'
 					n[d.decode('utf-8')] = ''
@@ -165,21 +198,24 @@ def matchgenre(imdb_genre,genres,genres_path):
 	if genres.get(imdb_genre):
 		p = genres_path+genres.get(imdb_genre)+"/"
 		if isdir(p):
-			return genres_path+genres.get(imdb_genre)+"/"
+			return p
 	else:
 	# match folders
+		p = genres_path+imdb_genre+'/'
 		g = listdir(genres_path)
 		r = [m.group(0) for l in g for m in [re.compile("^("+imdb_genre+").*",re.I).search(l)] if m]
 		if len(r)==1:
-			if isdir(genres_path+r[0]+'/'):
-				return genres_path+r[0]+'/'
+			p = genres_path+r[0]+'/'
+			if isdir(p):
+				return p
+		print '!!!!! Target genre dirrecoty ['+p+'] does not exist.'
 	# match None	
 	return None
 
 
 def argus():
-	usage = 'Usage: %prog [-n] [-c /path/to/config|-r /path/to/movie_repo -g /path/to/genre]|[-t todo.lst]'
-        version = 'v0.3'
+	usage = 'Usage: %prog [options]'
+        version = 'v0.4'
 
         parser = OptionParser(usage=usage,version="%prog "+version)
         parser.add_option("-n","--dry-run", action="store_true", dest="display", default=False, help="display only, do no harm")
@@ -188,14 +224,15 @@ def argus():
         group1.add_option("-r","--repository",type="string",dest="movies_repo",metavar="/path/to/movie_repo", help="the path to movies repository")
         group1.add_option("-g","--genres_path",type="string",dest="genres_path",metavar="/path/to/genre", help="the path to genres directory")
         parser.add_option_group(group1)
-        group2 = OptionGroup(parser,"Use Todo List to Parser Movies")
-        group2.add_option("-t","--todo", type="string", dest="todo", metavar="/path/to/todo.lst", default="todo.lst", help=" the path to todo list file, default todo list file is [%default]")
+        group2 = OptionGroup(parser,"Todo & Ignore Lists")
+        group2.add_option("-t","--todo", type="string", dest="todo", metavar="/path/to/todo.lst", default="GM.todo", help="the path to todo list file, default todo list file is [%default]")
+	group2.add_option("-i","--ignore", type="string", dest="ignore", metavar="/path/to/ignore.list", default="GM.ignore", help="the path to ignore list file, default ignore list file is [%default]")
         parser.add_option_group(group2)
 
 	return parser
 
 def main():
-	
+	#print '\tRunning as user ['+getpass.getuser()+'].'	# comment
 	parser = argus()
 	(opts, args) = parser.parse_args()
 	#print opts.display,opts.config,opts.movies_repo,opts.genres_path	# comment
@@ -204,11 +241,11 @@ def main():
 		confile = opts.config
 	else:
 		confile = 'GM.cfg'
-        config = ConfigParser.RawConfigParser()
         if not isfile(confile):
-                print 'I can not find config file '+confile+'. You can adopt example file GM.cfg.example and rename it to default config file GM.cfg. Exiting ...'
+                print '!!!!! I can not find config file '+confile+'. You can adopt example file GM.cfg.example and rename it to default config file GM.cfg. Exiting ...'
                 sys.exit(errno.ENOENT)
 
+	config = ConfigParser.RawConfigParser()
         config.read(confile)
         if opts.movies_repo:
 		movies_repo = opts.movies_repo
@@ -218,14 +255,40 @@ def main():
 		genres_path = opts.genres_path
 	else:
 		genres_path = config.get('Path','genre_path').encode('UTF8')
-        genres = dict(config.items('Genre'))
+	try:
+		cd = config.get('Path','Display').lower()
+	except:
+		cd = opts.display
+	else:
+		opts.display = cd
+	#print 'opts.display = '+str(opts.display)	# comment
+
+	genres = dict(config.items('Genre'))
         #print genres		# comment
+	try:
+		todofile = config.get('Path','TODO')
+	except:
+		todofile = 'GM.todo'
+	try:
+		ignorefile = config.get('Path','IGNORE')
+	except:
+		ignorefile = 'GM.ignore'
+        if opts.todo:
+                todofile = opts.todo
+        if isfile(todofile):
+                with open(todofile) as t:
+                        todo = [line.rstrip('\n') for line in t]
+        if opts.ignore:
+                ignorefile = opts.ignore
+        if isfile(ignorefile):
+                with open(ignorefile) as t:
+                        ignore = [line.rstrip('\n') for line in t]
 
         #m = str(raw_input('Movie Name: '))
         #y = str(raw_input('Movie Year: '))
         #res = querymovie(title=m, year=y)
         
-	y,n = searchdir(movies_repo,opts.display)
+	y,n = searchdir(movies_repo,opts.display,ignore)
 
         #print 'we have processed the following '+str(len(y))+' movies.'
 	#ppprint().pprint(y.keys())
@@ -239,25 +302,25 @@ def main():
 					#print linkd+m		# comment
 					if not isfile(linkd+m):
 						if opts.display:
-							print 'Link ['+linkd+m+'] would be created for movie ['+m+'].'
+							print '	Link ['+linkd+m+'] would be created for movie ['+m+'].'	#comment
 						else:
 							if not islink(linkd+m):
 								symlink(movies_repo+m,linkd+m)
 								# print 'Link ['+linkd+m+'] is created for movie ['+m+'].'
 							#else:
 								# print 'Link ['+linkd+m+'] exists.'
-
-	print '\nwe find no IMDB info for the following '+str(len(n))+' movies,'
-	#ppprint().pprint(n.keys())
-	if opts.display:
-		td = 'todo.mock.lst'
-	else:
-		td = 'todo.lst'
-	f = open(td,'w')
-	for m in n:
-		print m.encode("utf-8")
-		f.write(join(movies_repo,m.encode("utf-8"),'\n'))
-	f.close()
+				else:
+					print '!!!!! Target genre dirrecoty ['+g.decode("utf-8")+'] does not exist for movie ['+m.decode("utf-8")+'].'
+	if len(n)>0:
+		print '\n!!!!! I find no IMDB info for the following '+str(len(n))+' movies,'
+		#ppprint().pprint(n.keys())
+		if opts.display:
+			td = 'GM.todo.mock'
+		f = open(todofile,'w')
+		for m in n:
+			print m.encode("utf-8")
+			f.write(join(movies_repo,m.encode("utf-8"),'\n'))
+		f.close()
 
 if __name__ == '__main__':
 	main()
